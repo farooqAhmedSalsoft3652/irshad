b88afb7cc335eb43e5adde1aab92d1d36f9d8b04
farooqAhmedSalsoft3652 <farooq.a@salsoft.net>
2025-06-11 14:25:00 +0500

    admin Auth Layout

--- src/Components/Layouts/AdminLayout/Auth/index.jsx
+++ src/Components/Layouts/AdminLayout/Auth/index.jsx
@@ -1,51 +1,70 @@
 import { Link, useLocation } from "react-router-dom";
 import "./style.css";
 import { images } from "../../../../Assets";
+import { Col, Container, Row } from "react-bootstrap";
 
 export const AuthLayout = (props) => {
   const location = useLocation();
   const pathname = location?.pathname;
   const [, firstSegment] = pathname.split("/");
-  console.log(firstSegment, "firstSegment")
+  console.log(firstSegment, "firstSegment");
 
   return (
     <>
-      <section className={`authBg ${props?.authClasMain}`}>
-        <div className="container-fluid">
-          <div className="row gap-xl-5 gap-lg-3 justify-content-center authBox align-items-center">
-            <div className="col-lg-5 col-xxl-4 d-none d-lg-block ">
-              <div className="authLogo ">
-                <img src={images.adminLogo} alt="authLogo" />
-              </div>
-              <div className="auth-Bg">
-                <div className="auth-wrap  text-white">
-                  <p className="auth_card_title">{props?.authLeftText}</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-5 col-xxl-4">
-              <div className="authFormWrapper">
-                <div className="authForm">
-                  {/* <div className="authLogo ">
-                    <img src={images.adminLogo} alt="authLogo" />
-                  </div> */}
-                  <div className="authFormHeader mb-sm-5 mb-3 text-center">
-                    <h2 className="authTitle">{props?.authTitle}</h2>
-                    {props?.authMain && <p className="authPara ">{props?.authPara}</p>}
-                  </div>
-                  {props?.children}
-                  {props?.backOption && (
-                    <div className="text-center mt-5">
-                      <Link to={`/${firstSegment}`} className="fw-normal text-decoration-underline grayColor">
-                        <button className="notButton underlineOnHover blueColor">Back to Login</button>
-                      </Link>
+      <section className={`admin-login-wrap ${props?.authClasMain}`}>
+        <Container fluid>
+          <Row>
+            <Col xs={12} xl={10} className="mx-auto">
+              <div className="login-card p-0">
+                <Row className="justify-content-center g-0">
+                  <Col xs={12} lg={6} className="d-flex align-items-stretch">
+                    <div className="left position-relative w-100 ">
+                      <img
+                        src={images.authLeftImg}
+                        alt="authLogo"
+                        className="img-fluid"
+                      />
+                      <div className="position-absolute top-50 start-50 translate-middle">
+                        <img src={images.Logo} alt="authLogo" className="" />
+                      </div>
                     </div>
-                  )}
-                </div>
+                  </Col>
+                  <Col
+                    xs={12}
+                    lg={6}
+                    className="d-flex flex-column justify-content-center"
+                  >
+                    <div className="right">
+                      <div className="authFormWrapper">
+                        <div className="authForm">
+                          <div className="authFormHeader mb-sm-5 mb-3 text-center">
+                            <h2 className="authTitle">{props?.authTitle}</h2>
+                            {props?.authMain && (
+                              <p className="authPara ">{props?.authPara}</p>
+                            )}
+                          </div>
+                          {props?.children}
+                          {props?.backOption && (
+                            <div className="text-center mt-5">
+                              <Link
+                                to={`/${firstSegment}`}
+                                className="fw-normal text-decoration-underline grayColor"
+                              >
+                                <button className="notButton underlineOnHover blueColor">
+                                  Back to Login
+                                </button>
+                              </Link>
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </Col>
+                </Row>
               </div>
-            </div>
-          </div>
-        </div>
+            </Col>
+          </Row>
+        </Container>
       </section>
     </>
   );
